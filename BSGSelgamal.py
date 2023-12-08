cade01968c99ea5c6039ef2e8e801c7f74f56f62
gilbertdominics <113124448+gilbertdominics@users.noreply.github.com>
2023-12-08 12:32:41 -0500

    BSGSelgamal.py

--- BSGSelgamal.py
+++ BSGSelgamal.py
@@ -46,3 +46,31 @@ if pow(alpha, a, p) == beta:
     print("Decrypted message (m):", m)
 else:
     print("Verification failed.")
+
+def decimal_to_text(decimal_input, encoding='utf-8'): 
+    # Convert decimal to hex
+    hex_string = format(decimal_input, 'x')
+
+    # Ensure the hex string has an even number of characters
+    hex_string = '0' + hex_string if len(hex_string) % 2 else hex_string
+
+    # Convert hex to bytes
+    bytes_data = bytes.fromhex(hex_string)
+
+    # Try to convert bytes to string using the specified encoding
+    try:
+        return bytes_data.decode(encoding)
+    except UnicodeDecodeError:
+        return "Decoded bytes do not form a valid string in " + encoding + " encoding"
+
+# Example usage with different encodings
+decimal_output = m  # Replace with your actual decimal output
+
+# Try UTF-8
+print("UTF-8:", decimal_to_text(decimal_output, 'utf-8'))
+
+# If UTF-8 doesn't work, try ASCII
+print("ASCII:", decimal_to_text(decimal_output, 'ascii'))
+
+# Try ISO-8859-1
+print("ISO-8859-1:",decimal_to_text(decimal_output, 'iso-8859-1'))
